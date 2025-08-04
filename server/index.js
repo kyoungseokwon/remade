ba789ac8c24852eb0ef5332d60c897ed4b7811d4
kyoungseokwon <ksw15400@daum.net>
2025-08-04 22:48:53 +0900

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
+// server/index.js
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 10000;
+const port = process.env.PORT || 10000;
 
 app.get('/', (req, res) => {
-  res.send('리메이드 예약 서버 작동 중!');
+  res.send('리메이드 서버 정상 작동 중!');
 });
 
-app.listen(PORT, () => {
-  console.log(`✅ 서버 실행 중: http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`✅ 서버 실행 중: http://localhost:${port}`);
 });
